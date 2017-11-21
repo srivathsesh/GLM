66c5ae297ffa546ce6497d93d189f6501b949e03
Sri Seshadri <srivathsesh@gmail.com>
2017-11-21 07:05:08 -0500

    Corrected imputation to what I said I was going to do in the documentation Section 4.2. Corrected VIM error

--- Unit3_DeploymentCode_Seshadri.R
+++ Unit3_DeploymentCode_Seshadri.R
@@ -8,20 +8,20 @@ winedata.test <- read.csv("wine_test.csv")
 
 # impute data
 winedata.test.clean <- winedata.test[,-2]
-winedata.test.clean <- VIM::kNN(winedata.test.clean)
-
+#winedata.test.clean <- VIM::kNN(winedata.test.clean)
+winedata.test.clean <- winedata.test %>% mutate(STARS = ifelse(is.na(STARS), 0, STARS))
 # predict TARGET from model object - for verification purpose
-TARGET <- round(predict(poisson.zif.mdl2, newdata = winedata.test.clean, type = "response"),0)
-hist(TARGET)
+# TARGET <- round(predict(poisson.zif.mdl1, newdata = winedata.test.clean, type = "response"),0)
+# hist(TARGET)
 
 # model deployment from logistic model to predict zero inflation
-eta <- 22.15 - 23.7 * winedata.test.clean$STARS
+eta <- 11.5 - 13.13 * winedata.test.clean$STARS
 prob.no.sale <- (1 + exp(-1*eta))^-1
 prob.sale <- 1 - prob.no.sale
 plot(prob.sale)
 
 # predict counts
-predictedcounts <- round(exp(1.33 + 0.25* winedata.test.clean$LabelAppeal) *  prob.sale,0)
+predictedcounts <- round(exp(1.08 + 0.21* winedata.test.clean$LabelAppeal + 0.12 * winedata.test.clean$STARS) *  prob.sale,0)
 hist(predictedcounts)
 
 # writing predicted counts to file
